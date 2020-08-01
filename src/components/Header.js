37353a2f97b2a2b32693270531aa439de9c1a250
Carlos Castillo <crc8109@pm.me>
2020-08-01 18:08:35 -0400

    Netlify test branch (#11)
    * Added link to US tracker
    
    * Fixed typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,20 +8,14 @@ const Header = () => {
     <header>
       <Container type="content">
         <p>Global COVID19 Tracker</p>
-
+        <ul>
+          <li>
+            <a href="https://covid19-us-tracker.netlify.app/">US Tracker</a>
+          </li>
+        </ul>
       </Container>
     </header>
   );
 };
 
 export default Header;
-//Need to make this the link to the US_Map
-// <ul>
-//   <li>
-//     <Link to="/">Home</Link>
-//   </li>
-
-// <li>
-//   <Link to="/page-2/">Page 2</Link>
-// </li>
-// </ul>
